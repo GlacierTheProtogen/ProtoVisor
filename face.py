0a5b56fd4d4d8a30db8d8f843b94646de5fe7db3
GlacierTheProtogen <glacierproto@gmail.com>
2023-06-17 19:31:51 -0700

    Multithreading

--- face.py
+++ face.py
@@ -1,21 +1,57 @@
 #!/usr/bin/env python3
 from samplebase import SampleBase
+from pynput.keyboard import Key, Listener
 import numpy as np
+import json
+import time
 
-face = np.loadtxt('faces/baseface.txt', dtype="str", delimiter=None)
-print(face)
+pressedkey='0'
+
+face1 = np.loadtxt('all-faces/baseface.txt', dtype="str", delimiter=None)
+face2 = np.loadtxt('all-faces/uwu.txt', dtype="str", delimiter=None)
+face3 = np.loadtxt('all-faces/happy.txt', dtype="str", delimiter=None)
+face4 = np.loadtxt('all-faces/heart.txt', dtype="str", delimiter=None)
+face5 = np.loadtxt('all-faces/poker.txt', dtype="str", delimiter=None)
+face6 = np.loadtxt('all-faces/blink.txt', dtype="str", delimiter=None)
+
+def on_press(key):
+  pressedkey=key
+
+def on_release(key):
+  pressedkey='0'
+
 
 class SimpleSquare(SampleBase):
     def __init__(self, *args, **kwargs):
         super(SimpleSquare, self).__init__(*args, **kwargs)
 
     def run(self):
+
+        listener = Listener(
+           on_press=on_press,
+           on_release=on_release)
+        listener.start()
+
         offset_canvas = self.matrix.CreateFrameCanvas()
         while True:
-            offset_canvas.SetPixel(0, 50, 0, 0, 255)
-            offset_canvas.SetPixel(0, 0, 1, 1, 255)
-            offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
-
+          face = face1
+          if pressedkey[0] == 'q':
+            face = face2
+          elif pressedkey[0] == 'a':
+            face = face3
+          elif pressedkey[0] == 's':
+            face = face4
+          elif pressedkey[0] == 'w':
+            face = face5
+          for i in range(64):
+            for j in range(32):
+              if face[j][i] == 'X':
+                offset_canvas.SetPixel(i, j, 0, 0, 255)
+                offset_canvas.SetPixel(127-i, j, 0, 0, 225)
+              else:
+                offset_canvas.SetPixel(i, j, 0, 0, 0)
+                offset_canvas.SetPixel(127-i, j, 0, 0, 0)
+          offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
 # Main function
 if __name__ == "__main__":
