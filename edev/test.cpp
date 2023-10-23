9ad45363926ed57a33721f8c1dbb7776fdfd5829
GlacierTheProtogen <glacierproto@gmail.com>
2023-10-23 02:54:27 +0100

    Basic Xbox 360 input and cleanup

--- edev/test.cpp
+++ edev/test.cpp
@@ -4,27 +4,41 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+
+void process_events(struct libevdev *dev) {
+
+	struct input_event ev = {};
+	int status = 0;
+	auto is_error = [](int v) { return v < 0 && v != -EAGAIN; };
+	auto has_next_event = [](int v) { return v >= 0; };
+	const auto flags = LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING;
+
+	while (status = libevdev_next_event(dev, flags, &ev), !is_error(status)) {
+		if (!has_next_event(status)) continue;
+
+		std::cout << "Got input_event";
+		std::cout << " type=" << ev.type;
+		std::cout << " code=" << ev.code;
+		std::cout << " value=" << ev.value << std::endl;
+	}
+}
+
+
 int main() {
 	struct libevdev *dev = nullptr;
 
-	for (int i = 0;; i++) {
-		std::string path = "/dev/input/event" + std::to_string(i);
-		int fd = open(path.c_str(), O_RDWR|O_CLOEXEC);
-		if (fd == -1) {
-			break; // no more character devices
-		}
-		if (libevdev_new_from_fd(fd, &dev) == 0) {
-			//std::string phys = libevdev_get_phys(dev);
-			std::string name = libevdev_get_name(dev);
+	std::string path = "/dev/input/event0";
+	int fd = 0;
 
-			std::cout << path << std::endl;
-			//std::cout << "- phys: " << phys << std::endl;
-			std::cout << "- name: " << name << std::endl;
-			std::cout << std::endl;
-
+	while(1) {
+		fd = open(path.c_str(), O_RDWR|O_CLOEXEC);
+    		if (libevdev_new_from_fd(fd, &dev) == 0) {
+			libevdev_grab(dev, LIBEVDEV_GRAB);
+			process_events(dev);
 			libevdev_free(dev);
 		}
 		close(fd);
 	}
+
 	return 0;
 }
