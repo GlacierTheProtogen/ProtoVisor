8fe77d7140314f3feb22d7a2bbe028842dc47ac7
GlacierTheProtogen <glacierproto@gmail.com>
2023-10-30 00:20:36 +0000

    Progress

--- edev/test.cpp
+++ edev/test.cpp
@@ -7,82 +7,170 @@
 #include <unistd.h>
 
 // 1,000,000 / 30 = 33333
-int mseconds = 33333;
+const int mseconds = 33333;
+const int frames = 60;
+
+//Arbitrary, just needs to be mapped to /etc/xboxdrv/drv-player1/2.conf
+const int X1 = 34;
+const int A1 = 20;
+const int Y1 = 35;
+const int B1 = 21;
+const int DPAD_UP_1 = 46;
+const int DPAD_DOWN_1 = 47;
+const int DPAD_LEFT_1 = 44;
+const int DPAD_RIGHT_1 = 45;
+const int JOYS_UP_1 = 30;
+const int JOYS_DOWN_1 = 31;
+const int JOYS_LEFT_1 = 16;
+const int JOYS_RIGHT_1 = 17;
+const int GUIDE_1 = 2;
+const int START_1 = 5;
+const int BACK_1 = 4;
+
+const int X2 = 38;
+const int A2 = 24;
+const int Y2 = 48;
+const int B2 = 25;
+const int DPAD_UP_2 = 36;
+const int DPAD_DOWN_2 = 37;
+const int DPAD_LEFT_2 = 22;
+const int DPAD_RIGHT_2 = 23;
+const int JOYS_UP_2 = 9;
+const int JOYS_DOWN_2 = 10;
+const int JOYS_LEFT_2 = 49;
+const int JOYS_RIGHT_2 = 50;
+const int GUIDE_2 = 3;
+const int START_2 = 7;
+const int BACK_2 = 6;
 
 
-void counter(int* arr)
-{
-	while(1) {
-	  for(int i = 0; i < 8; i++)
-	  {
-	    if(arr[i] != 0)
-	    {
-	      arr[i] = arr[i] - 1;
-	    }
-	  }
-	  usleep(mseconds);
-	}
-}
 
-
-void process_events(struct libevdev *dev, int* arr) {
+void process_events(struct libevdev *dev, int* arr, std::string device) {
 
 	struct input_event ev = {};
 	int status = 0;
 	auto is_error = [](int v) { return v < 0 && v != -EAGAIN; };
-	auto has_next_event = [](int v) { return v >= 0; };
 	const auto flags = LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING;
-	//const auto flags = LIBEVDEV_READ_FLAG_NORMAL;
 
-	//std::thread countert(counter, arr);
-	//countert.join();
+	int X;
+	int A;
+	int Y;
+	int B;
+	int DPAD_UP;
+	int DPAD_DOWN;
+	int DPAD_LEFT;
+	int DPAD_RIGHT;
+	int JOYS_UP;
+	int JOYS_DOWN;
+	int JOYS_LEFT;
+	int JOYS_RIGHT;
+	int GUIDE;
+	int START;
+	int BACK;
 
-	//status = libevdev_next_event(dev, flags, &ev);
+	if(device == "event0")
+	{
+	  X = X1;
+	  A = A1;
+	  Y = Y1;
+	  B = B1;
+	  DPAD_UP = DPAD_UP_1;
+	  DPAD_DOWN = DPAD_DOWN_1;
+	  DPAD_LEFT = DPAD_LEFT_1;
+	  DPAD_RIGHT = DPAD_RIGHT_1;
+	  JOYS_UP = JOYS_UP_1;
+	  JOYS_DOWN = JOYS_DOWN_1;
+	  JOYS_LEFT = JOYS_LEFT_1;
+	  JOYS_RIGHT = JOYS_RIGHT_1;
+	  GUIDE = GUIDE_1;
+	  START = START_1;
+	  BACK = BACK_1;
+	}
+	else
+	{
+	  X = X2;
+	  A = A2;
+	  Y = Y2;
+	  B = B2;
+	  DPAD_UP = DPAD_UP_2;
+	  DPAD_DOWN = DPAD_DOWN_2;
+	  DPAD_LEFT = DPAD_LEFT_2;
+	  DPAD_RIGHT = DPAD_RIGHT_2;
+	  JOYS_UP = JOYS_UP_2;
+	  JOYS_DOWN = JOYS_DOWN_2;
+	  JOYS_LEFT = JOYS_LEFT_2;
+	  JOYS_RIGHT = JOYS_RIGHT_2;
+	  GUIDE = GUIDE_2;
+	  START = START_2;
+	  BACK = BACK_2;
+	}
 
 	while(1) {
-	//while (status = libevdev_next_event(dev, flags, &ev), !is_error(status)) {
-	//	if (!has_next_event(status)) {
-	//	  continue;
-	//	}
 		//std::cout << "Got input_event";
 		//std::cout << " type=" << ev.type;
 		//std::cout << " code=" << ev.code;
 		//std::cout << " value=" << ev.value << std::endl;
 
 
-		//status = libevdev_next_event(dev, flags, &ev);
-
 		if(libevdev_has_event_pending(dev))
 		{
-
-		  std::cout << "Event detected" << std::endl;
 
 		  status = libevdev_next_event(dev, flags, &ev);
 		  if(!is_error(status))
 	 	  {
-
-		    if(has_next_event(status)) {
-		      if(ev.code == 20)
-		      {
-		        arr[1] = 40;
-		      }
-		    }
+		      swich(ev.code)
+		      case X:
+			arr[0] = frames;
+			break;
+		      case A:
+			arr[1] = frames;
+			break;
+		      case Y:
+			arr[2] = frames;
+		        break;
+		      case B:
+			arr[3] = frames;
+			break;
+		      case DPAD_UP:
+			arr[4] = frames;
+			break;
+		      case DPAD_DOWN:
+			arr[5] = frames;
+			break;
+		      case DPAD_LEFT:
+			arr[6] = frames;
+			break;
+		      case DPAD_RIGHT:
+			arr[7] = frames;
+		        break;
+		      case JOYS_UP:
+			arr[8] = frames;
+			break;
+		      case JOYS_DOWN:
+			arr[9] = frames;
+			break;
+		      case JOYS_LEFT:
+			arr[10] = frames;
+			break;
+		      case JOYS_RIGHT:
+			arr[11] = frames;
+		        break;
+		      case GUIDE:
+			arr[12] = frames;
+			break;
+		      case START:
+			arr[13] = frames;
+			break;
+		      case BACK:
+			arr[14] = frames;
+			break;
 		  }
 		}
 
-		std::cout << "None" << std::endl;
-
-		if(ev.code == 20)
+		for(int i = 0; i < 15; i++)
 		{
-		  arr[1] = 40;
+			arr[i] = arr[i] - 1;
 		}
-
-		if(arr[1] != 0)
-		{
-		  arr[1] = arr[1] - 1;
-		}
-
-		usleep(mseconds/3);
 	}
 }
 
@@ -98,48 +186,34 @@ void controller(std::string device, int* arr) {
 		fd = open(path.c_str(), O_RDWR|O_CLOEXEC);
 		if (libevdev_new_from_fd(fd, &dev) == 0) {
 			libevdev_grab(dev, LIBEVDEV_GRAB);
-			process_events(dev, arr);
+			process_events(dev, arr, device);
 			libevdev_free(dev);
 		}
 		close(fd);
 	}
 
-
 }
 
 
-void reader(int *arr)
+void reader(int* arr)
 {
-	while(1){
-	  //for(int i = 0; i < 8; i++)
-	  //{
-	  //  std::cout << arr[i];
-	  //}
-	  std::cout << arr[1];
-	  std::cout << std::endl;
-	  usleep(mseconds);
+	
 
-	}
 }
-
-
-
 
 int main() {
 
-        int *arr = new int[8];
+        int *controller1buttons = new int[15];
+	int *controller2buttons = new int[15];
 
-	for(std::size_t i = 0; i < 8; i++){
-		arr[i] = false;
-	}
 
-	std::thread controller1(controller, "event0", arr);
-	//std::thread repeater1(reader, arr);
-	//std::thread controller2(controller, "event1", arr);
+	std::thread controller1(controller, "event0", controller1buttons);
+	std::thread controller2(controller, "event1", controller2buttons);
+	std::thread reader(controller1buttons);
 
 	controller1.join();
-	//repeater1.join();
-	//controller2.join();
+	controller2.join();
+	reader.join();
 
 
  	return 0;
